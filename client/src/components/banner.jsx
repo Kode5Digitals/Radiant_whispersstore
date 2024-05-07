ba1f1e071d07cb6c78071cef959a92b3a5180416
esther odedoyin <estherodedoyin1@example.com>
2024-05-07 16:35:01 +0100

    updated

--- client/src/components/banner.jsx
+++ client/src/components/banner.jsx
@@ -17,9 +17,8 @@ const Banner = () => {
                  Envelop yourself in the luxurious embrace of nature purity, where the essence of cream whispers tales of indulgence and revitalization
                  </p>
                </div>
-            
               <div className="flex relative">
-              <div className="home-img1 overflow-hidden " ><img  src="./public/images/cream6.jpeg"  className="w-full h-full " alt=""></img></div>
+              <div className="home-img1 overflow-hidden " ><img  src="images/cream1.jpeg"  className="w-full h-full " alt=""></img></div>
                <div className="home-img1 overflow-hidden  absolute left-72 bottom-12" ><img  src="./public/images/cream2.jpeg"  className="w-full h-full " alt=""></img></div>
               </div>
          </div>
