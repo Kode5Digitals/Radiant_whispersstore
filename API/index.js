d126b3e2556d2d003ba504ffc1338709ee54c988
esther odedoyin <estherodedoyin1@example.com>
2024-05-19 21:20:26 +0100

    added frontend link

--- API/index.js
+++ API/index.js
@@ -14,7 +14,7 @@ app.use(cookiesparser())
 
 app.use(cors(
     {
-    origin: 'http://localhost:5173',
+    origin:  'https://radiant-whispersstore-nine.vercel.app',
     credentials: true
   }
 ));
