226e141c37a2715b9d1a3b98387c9dc5615cb4d6
esther odedoyin <estherodedoyin1@example.com>
2024-05-29 08:08:51 +0100

    auth

--- API/config/env.js
+++ API/config/env.js
@@ -7,17 +7,22 @@ Cloud_name,
 CloudApi_key,
 CloudApi_secret ,
 PAYSTACK_PUBLIC_KEY,
-REFRESH_TOKEN_PUBLIC_KEY,
-REFRESH_TOKEN_PRIVATE_KEY
+// REFRESH_TOKEN_PUBLIC_KEY,
+// REFRESH_TOKEN_PRIVATE_KEY,
+REFRESH_TOKEN_SECRET,
+JWT_KEY
 } = process.env
 module.exports = {
     PORT,
     jwtKey,
+    REFRESH_TOKEN_SECRET,
+    JWT_KEY,
     CONNECTION_URL,
     Cloud_name,
     CloudApi_key,
     CloudApi_secret,
     PAYSTACK_PUBLIC_KEY,
-    REFRESH_TOKEN_PUBLIC_KEY,
-    REFRESH_TOKEN_PRIVATE_KEY
+    // REFRESH_TOKEN_PUBLIC_KEY,
+    // REFRESH_TOKEN_PRIVATE_KEY,
+
 }
