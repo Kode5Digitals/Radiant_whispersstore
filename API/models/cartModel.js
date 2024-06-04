63ab6b5dbc16087d112093ef240431cabb17d203
esther odedoyin <estherodedoyin1@example.com>
2024-06-04 15:43:25 +0100

    updated

--- API/models/cartModel.js
+++ API/models/cartModel.js
@@ -14,10 +14,19 @@ const cartSchema = new Schema({
   totalPrice: { type: Number, required: true, default: 0 }
 }, { timestamps: true });
 
+// cartSchema.methods.calculateTotals = function () {
+//   this.totalQuantity = this.products.reduce((acc, item) => acc + item.quantity, 0);
+//   this.totalPrice = this.products.reduce((acc, item) => acc + (item.quantity * Number(item.productId.price)), 0);
+// };
 cartSchema.methods.calculateTotals = function () {
-  this.totalQuantity = this.products.reduce((acc, item) => acc + item.quantity, 0);
-  this.totalPrice = this.products.reduce((acc, item) => acc + (item.quantity * Number(item.productId.price)), 0);
-};
+    const self = this;
+    return mongoose.model('Product').populate(this, 'products.productId')
+      .then(function(cart) {
+        self.totalQuantity = cart.products.reduce((acc, item) => acc + item.quantity, 0);
+        self.totalPrice = cart.products.reduce((acc, item) => acc + (item.quantity * item.productId.price), 0);
+      });
+  };
+  
 
 const Cart = mongoose.model('Cart', cartSchema);
 
