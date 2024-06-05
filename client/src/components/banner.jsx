1df05393f1206cab3ae62a06443d7a7b40b8e311
esther odedoyin <estherodedoyin1@example.com>
2024-06-05 17:18:02 +0100

    updated

--- client/src/components/banner.jsx
+++ client/src/components/banner.jsx
@@ -14,7 +14,7 @@ const Banner = () => {
     <div className="relative ">
 
     <div className="mt-36 xl:mt-[74px]  md:mt-[70px]" >
-       <div className=" w-full relative top-0   h-[400px] xl:h-[540px] lg:h-[400px] md:h-[380px]">
+       <div className=" w-full relative top-0   h-[400px] xl:h-[540px] lg:h-[400px] md:h-[440px]">
      <img src="RadiantWhisperBanner.png" alt="RadiantWhisperBanner" className="w-full h-full sm:hidden hidden xl:block lg:block md:hidden"/>
      <img src="RadiantWhispersstoreBanner2.png" alt="RadiantWhisperBanner"  className="block w-full h-full  sm:block xl:hidden lg:hidden md:block"/>
 <div className="absolute xl:top-24 top-16  right-[-30px] sm:right-[-44px] xl:pl-20 w-1/2 xl:left-[550px] 2xl:left-[700px] 2xl:pl-44   lg:right-[32px]  md:right-[-100px]   ">
