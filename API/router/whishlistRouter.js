91fa5d82170d69fad7453bfb5a448543d51f139c
esther odedoyin <estherodedoyin1@example.com>
2024-06-10 22:53:24 +0100

    database

--- API/router/whishlistRouter.js
+++ API/router/whishlistRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { getWhishlist, addWishlist, removeWhishlist } = require("../controllers/whishlist");
 const router = express.Router();
 
-router.get("/whishlists",getWhishlist);
+router.get("/whishlistsItems",getWhishlist);
 router.post("/addwhishlist",addWishlist)
 router.delete('/removewhishlist', removeWhishlist)
 module.exports = router;
