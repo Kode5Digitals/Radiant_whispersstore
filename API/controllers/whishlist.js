20a70e83b6c8f21008c04fecf5cf899db22e4b76
esther odedoyin <estherodedoyin1@example.com>
2024-06-10 23:23:34 +0100

    database

--- API/controllers/whishlist.js
+++ API/controllers/whishlist.js
@@ -1,4 +1,4 @@
-const whishlistModel = require("../models/whishlistModel");
+const wishlistModel = require("../models/whishlistModel");
 
 
 
@@ -6,7 +6,7 @@ const whishlistModel = require("../models/whishlistModel");
 const getWhishlist=async (req, res) => {
     const { userId, sessionId } = req.query;
     try {
-      const wishlists = await whishlistModel.find({ userId, sessionId });
+      const wishlists = await wishlistModel.find({ userId, sessionId });
       res.json({ message: 'Wishlists retrieved successfully', wishlists });
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -27,27 +27,27 @@ const getWhishlist=async (req, res) => {
       // First, try to find the wishlist by userId
       if (userId) {
         filter = { userId };
-        existingWishlist = await Wishlist.findOne(filter);
+        existingWishlist = await wishlistModel.findOne(filter);
       }
   
       // If no wishlist found by userId, try to find by sessionId
       if (!existingWishlist && sessionId) {
         filter = { sessionId };
-        existingWishlist = await whishlistModel.findOne(filter);
+        existingWishlist = await wishlistModel.findOne(filter);
       }
   
       // If no existing wishlist found, create a new one
       if (!existingWishlist) {
-        const newWishlist = new whishlistModel({ userId, sessionId, whishlists });
+        const newWishlist = new wishlistModel({ userId, sessionId, wishlists });
         const savedWishlist = await newWishlist.save();
         return res.json({ message: 'Wishlist created successfully', wishlist: savedWishlist });
       }
   
       // If an existing wishlist is found, update it
-      const update = { userId, sessionId, whishlists };
+      const update = { userId, sessionId, wishlists };
       const options = { new: true, upsert: true };
   
-      const upsertedWishlist = await Wishlistm.findOneAndUpdate(filter, update, options);
+      const upsertedWishlist = await wishlistModel.findOneAndUpdate(filter, update, options);
       res.json({ message: 'Wishlist updated successfully', wishlist: upsertedWishlist });
   
     } catch (error) {
@@ -60,7 +60,7 @@ const getWhishlist=async (req, res) => {
   const removeWhishlist= async (req, res) => {
   const { id } = req.params;
   try {
-    await whishlistModel.findByIdAndDelete(id);
+    await wishlistModel.findByIdAndDelete(id);
     res.json({ message: 'Wishlist deleted successfully' });
   } catch (error) {
     res.status(500).json({ error: error.message });
