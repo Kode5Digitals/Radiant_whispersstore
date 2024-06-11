751264b636c2112d74807b6e214c6e11e068de35
esther odedoyin <estherodedoyin1@example.com>
2024-06-11 20:01:58 +0100

    database

--- API/controllers/whishlist.js
+++ API/controllers/whishlist.js
@@ -48,7 +48,7 @@ const getWhishlist=async (req, res) => {
         model: 'Product' // This should match your Product model name
       });
       
-      res.json({ message: 'Item added to wishlist successfully', wishlists: populatedWishlist });
+      res.json({ message: 'Item added to wishlist successfully', populatedWishlist });
   
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -75,7 +75,7 @@ const removeWhishlist  = async (req, res) => {
 
     const savedWishlist = await wishlist.save();
     const populatedWishlist = await wishlistModel.findById(savedWishlist._id).populate('items.productId');
-    res.json({ message: 'Item removed from wishlist successfully', wishlists: populatedWishlist });
+    res.json({ message: 'Item removed from wishlist successfully',  populatedWishlist });
 
   } catch (error) {
     res.status(500).json({ error: error.message });
