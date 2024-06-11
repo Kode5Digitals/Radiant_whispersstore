3e22a6a5674280ed5da3364617fa1df9036274d9
esther odedoyin <estherodedoyin1@example.com>
2024-06-11 11:21:23 +0100

    database

--- API/controllers/whishlist.js
+++ API/controllers/whishlist.js
@@ -6,7 +6,10 @@ const wishlistModel = require("../models/whishlistModel");
 const getWhishlist=async (req, res) => {
     const { userId, sessionId } = req.query;
     try {
-      const wishlists = await wishlistModel.find({ userId, sessionId });
+      const wishlists = await wishlistModel.find({ userId, sessionId }).populate({
+        path: 'items.productId',
+        model: 'Product' // This should match your Product model name
+      });;
       console.log(wishlists)
       res.json({ message: 'Wishlists retrieved successfully', wishlists });
     } catch (error) {
