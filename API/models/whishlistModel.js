fc4bf7b6405e9f6add66febf26dbeb88dee17c0f
esther odedoyin <estherodedoyin1@example.com>
2024-06-16 19:38:20 +0100

    database

--- API/models/whishlistModel.js
+++ API/models/whishlistModel.js
@@ -6,7 +6,7 @@ const WishlistItemSchema = new Schema({
 });
 
 const WishlistSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref:"User",  required: function() { return !this.userId}},
+  userId: { type: Schema.Types.ObjectId, ref:"User",  required: function() { return !this.sessionId}},
   sessionId: { type: String,  required: function() { return !this.userId; }, unique: true, },
   items: [WishlistItemSchema],
 }, {
