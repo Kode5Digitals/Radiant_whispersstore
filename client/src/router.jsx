ac40484d59518a4246eb084dce592b05fe2f4335
esther odedoyin <estherodedoyin1@example.com>
2024-06-18 07:00:05 +0100

    vercel

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,6 +1,6 @@
 import React,  { lazy, Suspense } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
-import ProtectedRoute from './components/ProtectedRoute.js'
+import ProtectedRoute from '../src/components/ProtectedRoute.js'
 
 const NotFound = React.lazy(() => import('../src/pages/NotFound.jsx'))
 const App = lazy(() => import('./App.jsx'))
