4833e01ade98fa32bc9643d57149f3322f537abc
esther odedoyin <estherodedoyin1@example.com>
2024-09-03 13:40:42 +0100

    made changes to paystack

--- API/controllers/paystack.js
+++ API/controllers/paystack.js
@@ -68,7 +68,7 @@ const verifyPayment = async (req, res) => {
 const requestKey=async(req, res)=>{
   try{
 console.log(KEY)
-res.json({ data:KEY });
+res.status(200).json({ data:"paystack" });
   }
   catch(err){
     res.status(500).json({ message: 'Could not get Key', error: err });
