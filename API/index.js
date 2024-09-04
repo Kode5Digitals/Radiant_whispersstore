2e1654e32ea8a13ce59dfa330bf68d8dc8660e5e
esther odedoyin <estherodedoyin1@example.com>
2024-09-04 20:33:26 +0100

    paystack

--- API/index.js
+++ API/index.js
@@ -24,7 +24,8 @@ app.use(handler)
 
 const corsOptions = {
     origin: [
-      'https://radiant-whispersstore-nine.vercel.app',
+      // 'https://radiant-whispersstore-nine.vercel.app',
+      'https://radiantwhispersstore.com',
       "https://api.paystack.co",
       'http://localhost:5173'
     ],
