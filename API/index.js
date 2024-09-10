20a767e8661c85cad7f4ebfa11a80da2e52f4ff3
esther odedoyin <estherodedoyin1@example.com>
2024-09-10 20:43:13 +0100

    cors

--- API/index.js
+++ API/index.js
@@ -31,9 +31,8 @@ const corsOptions = {
       "https://api.paystack.co",
       'http://localhost:5173'
     ],
-    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
   };
 
 
